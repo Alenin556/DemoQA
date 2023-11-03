bf9ab96c2b2afdc4614607b868806a28d63370e2
Alenin Andrew <aleninmailbox@gmail.com>
2023-11-03 16:48:13 +0300

    test

--- src/test/java/Page.java
+++ src/test/java/Page.java
@@ -1,2 +1,40 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Page {
+    public WebDriver driver;
+
+    public Page(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[@text='text']")
+    public WebElement fieldName;
+
+    @FindBy(xpath = "//*[@id='userEmail']")
+    private WebElement fieldEmail;
+
+    @FindBy(id = "currentAddress")
+    private WebElement fieldCurrentAdr;
+
+    @FindBy(id = "permanentAddress")
+    private WebElement fieldPermamentAdr;
+
+    @FindBy(id = "submit")
+    private WebElement button;
+
+    @FindBy(name = "mobilePhone")
+    public WebElement fieldPhone;
+
+
+    public void createTextBox(String fieldNameValue, String fieldEmailValue, String currentAddressValue, String permanentAddressValue) {
+        fieldName.sendKeys(fieldNameValue);
+        fieldEmail.sendKeys(fieldEmailValue);
+        fieldCurrentAdr.sendKeys(currentAddressValue);
+        fieldPermamentAdr.sendKeys(permanentAddressValue);
+        button.click();
+    }
 }
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,73 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
 public class test {
+
+    private static WebDriver driver;
+    private static Page page;
+
+
+    @BeforeAll
+    // Перед началом написания тестов, инициализируем драйвер для взаимодействия с браузером.
+    // Инициализация (от англ. initialization, инициирование) — создание, подключение, активация, подготовка к работе, определение параметров.
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Java Works\\Netology Homeworks\\SeleniumPracticeMaven1\\driver\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options); //добавляем драйверу опции и методы по взаимодействию с объектами
+        driver.getWindowHandle(); // опция для проведения тестов в открытом окне браузера
+
+        // присваиваем переменной mainPage, объект класса MainPage, для взаимодействия с вебэлементами класса
+        page = new Page(driver);
+    }
+
+    @BeforeEach
+    void connect() {
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+//    @BeforeEach
+//    void connect() {
+//        driver.get("http://172.17.1.45:8081/login");
+//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+//    }
+
+    @Test
+    public void test() {
+        String fieldName = "Ilya";
+        String fieldEmail = "pichugin.lua@yandex.ru";
+        String fieldCurrentAdr = "Проспект Ленина";
+        String fieldPermamentAdr = "Проспект Лиги";
+        page.fieldName.click();
+        page.fieldName.sendKeys(fieldName);
+
+//        String fieldName = "Ilya";
+//        String fieldEmail = "pichugin.lua@yandex.ru";
+//        String fieldCurrentAdr = "Проспект Ленина";
+//        String fieldPermamentAdr = "Проспект Лиги";
+//        page.createTextBox(fieldName, fieldEmail, fieldCurrentAdr, fieldPermamentAdr);
+    }
+
+    @Test
+    public void test1() {
+        String fieldName = "Ilya";
+        page.fieldName.sendKeys(fieldName);
+    }
+
+    @Test
+    public void test2() {
+        String fieldClientPhone = "1234567";
+        page.fieldPhone.sendKeys(fieldClientPhone);
+    }
+
+
 }
+
+
